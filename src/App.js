b483d8a9a8e8eb0ffa817945164819614ee5a0b6
ariannemix <ariannejudkins@gmail.com>
2021-05-26 14:13:03 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import img from './img.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Dictionary">
+        <h1>Dictionary App</h1>
+        <img src={img} alt="Dictionary" className="Img" />
+        </div>
+    </div>
+  );
+}
+
+export default App;
