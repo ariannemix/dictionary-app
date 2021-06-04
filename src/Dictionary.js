f2dd248eaa3ebc712f5e90259ef281827764be85
ariannemix <ariannejudkins@gmail.com>
2021-06-04 15:11:42 -0400

    added images

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Images from "./Images";
 
 export default function Dictionary() {
 const [input, setInput] = useState("flower");
 let [results, setResults] = useState(null);
 let [loaded, setLoaded] = useState(false);
+let [images, setImages] = useState(null);
 
 
 
@@ -14,9 +16,17 @@ function handleResponse(response) {
     setResults(response.data[0]);
 }
 
+function handleImageResponse(response) {
+    setImages(response.data);
+}
+
 function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${input}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${input}&per_page=6`;
+    let apiKey = `563492ad6f917000010000019753ca1ee694469496038fb0afaa4818`;
+    axios.get(pexelsApiUrl, { headers: { "Authorization": `Bearer ${apiKey}` } }).then(handleImageResponse);
 }
 
     function handleSubmit(event) {
@@ -38,10 +48,12 @@ function search() {
                 </form>
             </div>
             <Results results={results} />
+            <Images images={images} alt={input} />
         </div>)
     } else {
         search();
         setLoaded(true);
     }
+    
     
 }
---
+++ src/Images.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Images.css";
+
+export default function Images(props) {
+    if (props.images) {
+        console.log(props.images);
+        return <div className="row Image">
+        {props.images.photos.map(function(image, index) {
+            return <div className="col-4">
+            <a href={image.src.original} target="_blank"><img key={index} src={image.src.tiny} alt={props.alt} class="img-fluid Pic" /></a>
+            </div>
+        })}
+            </div>
+    } else {
+        return null
+    }
+}
